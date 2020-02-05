ac2c8cb7576b1aa3e7c016b8e5b15cffbee8c729
yemecen <y.emecen@gmail.com>
2020-02-05 23:52:33 +0300

    Login

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,7 +1,17 @@
 <div class="row">
     <div class="col-md-12">
         <nav class="navbar navbar-light bg-light justify-content-between">
-            <a class="navbar-brand"></a>
+            <a class="navbar-brand" href="{{ route('logout') }}" 
+                    onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                    
+                    @if (Auth::user())
+                        {{Auth::user()->name }} - Çıkış
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    @endif                
+            </a>
             <form class="form-inline">
               <input class="form-control mr-sm-2" type="search" placeholder="Cm ID veya başlık..." aria-label="Search">
               <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Ara</button>
--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','CmController@index');
 
 Route::resource('cms','CmController');
+
+Auth::routes();
